1f69b026c82959c2dec4f8494d121e720cd1f683
Kasak Gupta <141016805+kasakgupta@users.noreply.github.com>
2026-08-16 23:20:58 +0530

    feat: implement refresh token regeneration in refreshToken function

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -96,6 +96,21 @@ export async function refreshToken(req, res) {
         }
     );
 
+    const newRefreshToken = jwt.sign({
+        id: decoded.id,
+    },
+        config.JWT_SECRET,
+        {
+            expiresIn: '7d'
+        }
+    );
+    res.cookie('refreshToken', newRefreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+    });
+    
     res.status(200).json({
         message: 'Token refreshed successfully',
         accessToken
